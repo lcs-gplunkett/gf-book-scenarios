bb5beecca92dd0b4ff9e16b67c40657b0b7fb249
Gavin Plunkett <gplunkett@lcs.on.ca>
2019-05-02 11:40:34 -0400

    made the image change

--- chapter07/autumn-1/Block.java
+++ chapter07/autumn-1/Block.java
@@ -1,5 +1,6 @@
 import greenfoot.*;
-
+//get access to the list type from the java API
+import java.util.List;
 /**
  * A block that bounces back and forth across the screen.
  * 
@@ -50,9 +51,24 @@ public class Block extends Actor
     {
         if (Greenfoot.mouseClicked(this)) 
         {
+            // 1. get an object reference
             World myWorld = getWorld();
             
-            myWorld.showText("clicked mouse", 200, 200);
+            //2. get a list of object references to all the leaf objects in the world
+            //TYPE, NAME,               CLASS
+            List<Leaf> leaves = myWorld.getObjects(Leaf.class);
+            
+            // 3. automatically iterate (loop) through the list the correct
+            //number of times.
+            //with each iteration, get an object reference to the next Leaf
+            //in the list
+            //"for each leaf in the leaves list..."
+            
+            for (Leaf leaf : leaves)
+            {
+             //change the image on this particular leaf
+             leaf.changeImage();
+            }
         }
     }
     
